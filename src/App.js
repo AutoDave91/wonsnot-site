8f5f62768b4dc3b59e43dd6286ee100e39e7728d
david <davidwnewman91@gmail.com>
2020-10-06 17:40:08 -0500

    yt embedded, twitch resisting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import './reset.css';
+import './responsive.css';
+import Home from './Components/Home/Home'
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      page: 'Home'
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e){
+    this.setState({page: e.target.value})
+  }
+  render(){
+    let {page} = this.state
+  
+    return (
+      <main className="App">
+        <section>
+          <Home />
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import './Home.css'
+
 function Home() {
-  return (
-    <main className='Home'>
-        <h1>Home</h1>
-    </main>
-  );
+    return (
+        <main className='Home'>
+            <header>
+                <img />
+                <nav>
+
+                </nav>
+            </header>
+            <h1>Home Component</h1>
+            <section className='gameplay'>
+                <span className="embed-youtube"><iframe className="youtube-player" src="https://www.youtube.com/embed/videoseries?list=PLCuDv3oRKHKsJQxDZNsqt6oXmkjV8TLHv&amp;hl=en_US" allowFullScreen={true} data-ratio="0.5625" data-width="560" data-height="315"></iframe></span>
+            </section>
+            <section className='VR'>
+                <span className="embed-youtube"><iframe className="youtube-player" src="https://www.youtube.com/embed/playlist?list=PLCuDv3oRKHKuHgYR2fk0f9NtCUGk8RJNN" allowFullScreen={true} data-ratio="0.5625" data-width="560" data-height="315"></iframe></span>
+            </section>
+            <section className='twitch_live'>
+                <iframe src="https://player.twitch.tv/?channel=mfpallytime&parent=davidwnewman.com" frameBorder="0" allowFullScreen={true} scrolling="no"></iframe><a href="https://www.twitch.tv/wonsnot?tt_content=text_link&tt_medium=live_embed">Watch live video from Wonsnot on www.twitch.tv</a>
+            </section>
+        </main>
+    );
 }
 
 export default Home;
